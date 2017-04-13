7c4b8a3194df3bd7b63aefefc89cb330f46f6f12
rindajones <rindajones@gmail.com>
2017-04-13 16:56:25 +0900

    Add bids.

--- features.R
+++ features.R
@@ -1,5 +1,6 @@
 source("init.R")
 
+### Global Variables ###
 db <- SQLite()
 con <- dbConnect(drv = db, fb.db)
 
@@ -7,10 +8,27 @@ con <- dbConnect(drv = db, fb.db)
 
 
 #### Start ###
+# Initial data.frames...
+
+# No good since column names are returned.
+# query <- "SELECT bidder_id, outcome FROM train"
+query <- "SELECT bidder_id FROM train"
+train  <- data.frame(bidder_id = dbGetQuery(con, query))
 query <- "SELECT bidder_id, outcome FROM train"
-tain  <- dbGetQuery(con, statement = query)
-#train <- dplyr::left_join(train, res, by="bidder_id")
+outcome  <- dbGetQuery(con, query)
+train <- dplyr::left_join(train, outcome, by="bidder_id")
 query <- "SELECT bidder_id FROM test"
-test  <- dbGetQuery(con, statement = query)
+test  <- data.frame(bidder_id = dbGetQuery(con, query))
+# Done.
+
+# The number of bids per bidder.
+# The query below replaced to bid_count to bids
+query <- paste0("SELECT bidder_id, COUNT(bidder_id) as bids ",
+                "FROM bids GROUP BY bidder_id")
+bids <- dbGetQuery(con, query)
+train <- dplyr::left_join(train, bids, by="bidder_id")
+test  <- dplyr::left_join(test, bids, by="bidder_id")
+# Done.
+
 
 dbDisconnect(con)
